90b5f2bc91363e2434ee6ae9df06c7ecd82cbe9b
Henderson-dev <teconet.arte@gmail.com>
2022-09-23 09:14:41 -0300

    Add base styles for sass

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,18 @@ import {Container, Row} from "react-bootstrap";
 
 export default function Header() {
   return (
+    <header>
       <Container>
         <Row>
           <div className="col-12">
-            teste
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="#">Como Funciona</a></li>
+              <li><a href="#">Login Artista</a></li>
+            </ul>
           </div>
         </Row>
       </Container>
+    </header>
   )
 }
