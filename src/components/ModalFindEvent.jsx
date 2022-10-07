677c4ec54bd172a5a8d5b36a645d8b53147a4ddf
Henderson-dev <teconet.arte@gmail.com>
2022-10-06 21:03:49 -0300

    Add modal find event

--- src/components/ModalFindEvent.jsx
+++ src/components/ModalFindEvent.jsx
@@ -1,17 +1,19 @@
 import { React, useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import useFetch from "../backend/useFetch";
+import { Link } from "react-router-dom";
 
 export default function ModalFindEvent(props) {
-  const [showGeoloc, setshowGeoloc] = useState(false);
-  const showOrHideGeoloc = () => setshowGeoloc(true);
+  const [showGeoloc, setshowGeoloc] = useState(0);
+  const [showCod, setshowCod] = useState(0);
+  const [showArtist, setshowArtist] = useState(0);
 
-  const [showCod, setshowCod] = useState(false);
-  const showOrHideCod = () => setshowCod(true);
+  // Call API page ID in Wordpress
+  let pathApiData = "evento/";
 
-  function GoWindow() {
-    const showOrHideGeoloc = () => setshowGeoloc(false);
-    return showOrHideGeoloc;
-  }
+  let dataFromPage = "wp-json/wp/v2/" + pathApiData;
+  const { data: pageData, error, isLoad } = useFetch(dataFromPage);
+  console.log(pageData);
 
   return (
     <Modal
@@ -24,29 +26,55 @@ export default function ModalFindEvent(props) {
       <Modal.Body>
         <h2>Localizar um evento</h2>
 
-        {showCod ? (<>Codigo</> ):(<></>)}
-        {showGeoloc ? (
+        {showGeoloc === 1 ? (
           <>
-            <p>Pela geo</p>
-            <button onClick={GoWindow()}>Voltar</button>
+            <h3>Pela geolocalização</h3>
+            {pageData.map((event) => {
+              return (
+                <>
+                  <ul>
+                    <li key={event.id}>
+                      <Link to={`/evento/${event.id}`}>{event.acf.nome_do_evento}</Link>
+                    </li>
+                  </ul>
+                </>
+              );
+            })}
+            <button onClick={() => setshowGeoloc(0)} className="btn-default">
+              Voltar
+            </button>
+          </>
+        ) : showCod === 1 ? (
+          <>
+            Codigo
+            <button onClick={() => setshowCod(0)} className="btn-default">
+              Voltar
+            </button>
+          </>
+        ) : showArtist === 1 ? (
+          <>
+            Artista
+            <button onClick={() => setshowArtist(0)} className="btn-default">
+              Voltar
+            </button>
           </>
         ) : (
           <div className="form-default">
             <div className="wrap-check">
               <div className="check-form">
-                <label onClick={() => setshowGeoloc(true)}>
+                <label onClick={() => setshowGeoloc(1)}>
                   <input type="radio" name="localizacao" />
                   <span className="wpcf7-list-item-label">
                     Pela minha geolocalização
                   </span>
                 </label>
-                <label onClick={() => setshowCod(true)}>
+                <label onClick={() => setshowCod(1)}>
                   <input type="radio" name="localizacao" />
                   <span className="wpcf7-list-item-label">
                     Pelo código do evento
                   </span>
                 </label>
-                <label onClick={showOrHideGeoloc}>
+                <label onClick={() => setshowArtist(1)}>
                   <input type="radio" name="localizacao" />
                   <span className="wpcf7-list-item-label">
                     Pelo nome do artista ou banda
